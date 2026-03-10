d7e244751dfe83cdc35bee61407d08ae30c2fe67
Oliver Antoniu <oliver+github@abolish.golf>
2026-03-10 14:32:23 -0600

    rev [07 - hooks]: ex01 - custom hook: write to React state AND sessionStorage

--- 07 - hooks + lifecycle/examples/01 - hooks/instructor/src/hooks/useSelectedResource.js
+++ 07 - hooks + lifecycle/examples/01 - hooks/instructor/src/hooks/useSelectedResource.js
@@ -1,5 +1,12 @@
 import { useState } from 'react';
 
+
+/* sessionStorage is a key-value store, built in to web behaviour (no need to import anything). 
+   Below, I'm defining which 'key' I'll be writing the resource value to.
+*/
+const STORAGE_KEY = 'selectedResource'
+
+
 /* This will be a custom hook built *around* useState.
 
    We're going to change a stateful variable, *but also*
@@ -18,9 +25,19 @@ export function useSelectedResource() {
 	function updateSelectedResource(resource) {
 		// 1. change the value in React state via setter
 		setSelectedResource(resource);
+
 		// 2. also write the value to persistent storage
-		// ...
-	}
+		// 2.a) if 'resource' is null, remove the key from sessionStorage entirely.
+		if (resource === null) {
+			sessionStorage.removeItem(STORAGE_KEY)
+		} else {
+		// 2.b) if 'resource' is/has data, write it to that key.
+			sessionStorage.setItem(
+				STORAGE_KEY,             // key in session storage
+				JSON.stringify(resource) // value in session storage. note: it's JSON!
+			)
+		}
+ 	}
 
 	return [selectedResource, updateSelectedResource]
 }
