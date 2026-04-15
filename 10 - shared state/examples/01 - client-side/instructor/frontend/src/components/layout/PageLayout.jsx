08195e064c743b1da5afcb7c5aee3d4c502be5f3
oaao <oliver+github@abolish.golf>
2026-04-15 16:03:10 -0600

    add [10 - shared state]: ex01 - static implementation of theming shared state

--- 10 - shared state/examples/01 - client-side/instructor/frontend/src/components/layout/PageLayout.jsx
+++ 10 - shared state/examples/01 - client-side/instructor/frontend/src/components/layout/PageLayout.jsx
@@ -1,6 +1,9 @@
-const theme = 'light';
+import { useContext } from 'react';
+import { ThemeContext } from '../../context/ThemeContext'
 
 export default function PageLayout({ header, children }) {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <div data-theme={theme} className="min-h-screen bg-base-200">
       <header className="border-b border-sky-600 bg-white px-6 py-4">
--- 10 - shared state/examples/01 - client-side/instructor/frontend/src/context/ThemeProvider.jsx
+++ 10 - shared state/examples/01 - client-side/instructor/frontend/src/context/ThemeProvider.jsx
@@ -3,6 +3,7 @@ import { ThemeContext } from './ThemeContext';
 
 export function ThemeProvider({ children }) {
   const [theme, setTheme] = useState('light');
+  // try changing default state to 'dark'
 
   function toggleTheme() {
     setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
